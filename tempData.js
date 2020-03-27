faa8f049139510ca74b3c13f16e0cf32b805fa89
Joonmyung <mytwiny@gmail.com>
2020-03-27 17:16:07 +0900

    Todo App Part 1 complete

---
+++ tempData.js
@@ -0,0 +1,60 @@
+export default tempData = [
+  {
+    name: "Plan a Trip",
+    color: "#24A6D9",
+    todos: [
+      {
+        title: "Book Flight",
+        completed: false
+      },
+      {
+        title: "Passport Check",
+        completed: true,
+      },
+      {
+        title: "Reserve Hotel Room",
+        completed: true
+      },
+      {
+        title: "Pack Luggage",
+        completed: false
+      }
+    ],
+  },
+  {
+    name: "Errands",
+    color: "#8022D9",
+    todos: [
+      {
+        title: "Buy Milk",
+        completed: true,
+      },
+      {
+        title: "Buy Make a video",
+        completed: true,
+      },
+      {
+        title: "Take dog for walk",
+        completed: true,
+      },
+    ],
+  },
+  {
+    name: "Birthday Party",
+    color: "#595BD9",
+    todos: [
+      {
+        title: "Get Balloons",
+        completed: false,
+      },
+      {
+        title: "Send Invitations",
+        completed: true,
+      },
+      {
+        title: "Make Dinner Reservation",
+        completed: true,
+      },
+    ],
+  },
+]
